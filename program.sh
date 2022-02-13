cf01344867e354847d558af5ae099e70716fb598
Wouter Peetermans <wouter@wouterpeetermans.com>
2022-02-13 23:26:17 +0100

    Fix typo

--- program.sh
+++ program.sh
@@ -6,8 +6,12 @@ echo "Feature 2";
 =======
 read -p "Give your name: " name;
 <<<<<<< master
+<<<<<<< master
 echo $name;
 >>>>>>> Add print name functionality to program
 =======
 echo "Hello $name. Greetings to you my friend";
 >>>>>>> Add personal greeting to program
+=======
+echo "Hello $name. Greetings to you my friend.";
+>>>>>>> Fix typo
